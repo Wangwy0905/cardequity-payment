6d197d5ab650b177c9d71be3e1bda62264db14ab
pqqanqing <15601870402@163.com>
2018-12-19 16:19:13 +0800

    panqingqing:bug fix

---
+++ cardequity-payment-biz/src/main/java/com/youyu/cardequity/payment/biz/help/handler/PayTradeRefundFactoryTypeHandler.java
@@ -0,0 +1,23 @@
+package com.youyu.cardequity.payment.biz.help.handler;
+
+import com.youyu.cardequity.common.orm.mybatis.handler.BaseString2StatusStrategyTypeHandler;
+import com.youyu.cardequity.payment.biz.component.factory.paytraderefund.PayTradeRefundFactory;
+import org.apache.ibatis.type.JdbcType;
+import org.apache.ibatis.type.MappedJdbcTypes;
+import org.apache.ibatis.type.MappedTypes;
+
+/**
+ * @author panqingqing
+ * @version v1.0
+ * @date 2018年12月6日 下午10:00:00
+ * @work 自定义类型
+ */
+@MappedJdbcTypes({JdbcType.VARCHAR})
+@MappedTypes({PayTradeRefundFactory.class})
+public class PayTradeRefundFactoryTypeHandler extends BaseString2StatusStrategyTypeHandler {
+
+    @Override
+    public String getClassSimpleName() {
+        return PayTradeRefundFactory.class.getSimpleName();
+    }
+}
---
+++ cardequity-payment-biz/src/main/java/com/youyu/cardequity/payment/biz/help/handler/PayTradeRefundStatusTypeHandler.java
@@ -0,0 +1,23 @@
+package com.youyu.cardequity.payment.biz.help.handler;
+
+import com.youyu.cardequity.common.orm.mybatis.handler.BaseString2StatusStrategyTypeHandler;
+import com.youyu.cardequity.payment.biz.component.status.paytraderefund.PayTradeRefundStatus;
+import org.apache.ibatis.type.JdbcType;
+import org.apache.ibatis.type.MappedJdbcTypes;
+import org.apache.ibatis.type.MappedTypes;
+
+/**
+ * @author panqingqing
+ * @version v1.0
+ * @date 2018年12月6日 下午10:00:00
+ * @work 自定义类型
+ */
+@MappedJdbcTypes({JdbcType.VARCHAR})
+@MappedTypes({PayTradeRefundStatus.class})
+public class PayTradeRefundStatusTypeHandler extends BaseString2StatusStrategyTypeHandler {
+
+    @Override
+    public String getClassSimpleName() {
+        return PayTradeRefundStatus.class.getSimpleName();
+    }
+}
