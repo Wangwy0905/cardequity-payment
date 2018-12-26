21d3f77168eb9ec98d696c4ca21f174743696a3e
pqqanqing <15601870402@163.com>
2018-12-26 17:56:59 +0800

    panqingqing:增加对账单下载。

--- cardequity-payment-biz/src/main/java/com/youyu/cardequity/payment/biz/help/util/FileUtil.java
+++ cardequity-payment-biz/src/main/java/com/youyu/cardequity/payment/biz/help/util/FileUtil.java
@@ -1,7 +1,5 @@
 package com.youyu.cardequity.payment.biz.help.util;
 
-import com.csvreader.CsvReader;
-
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -20,25 +18,6 @@ import static java.io.File.separator;
  * @work 文件工具类
  */
 public class FileUtil {
-
-    public static void main(String[] args) throws IOException {
-        unZipFile("/Users/panqingqing/Downloads/对账单/alipay/2018-12-11/2018-12-11.zip", "/Users/panqingqing/Downloads/对账单/alipay/2018-12-11", "GBK");
-
-        CsvReader csvReader = new CsvReader("/Users/panqingqing/Downloads/对账单/fund_bill_trade2018-12-19/20881021769041700156_20181219_业务明细.csv", ',', Charset.forName("GBK"));
-        // 读表头
-        csvReader.readHeaders();
-        int headerCount = csvReader.getHeaderCount();
-        while (csvReader.readRecord()) {
-            // 读一整行
-            System.out.println(csvReader.getRawRecord());
-//            int columnCount = csvReader.getColumnCount();
-//            for (int i = 0; i < columnCount; i++) {
-//                // 读这行的某一列
-//                System.out.println(csvReader.get(i));
-//            }
-        }
-
-    }
 
     public static void unZipFile(String fileName, String filePath, String charset) {
         File zipFile = new File(fileName);
