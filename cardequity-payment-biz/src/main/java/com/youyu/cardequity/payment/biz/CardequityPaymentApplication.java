3ac5d256ec1560ac1d2515ecd112f5121ebd35fe
pqqanqing <15601870402@163.com>
2018-12-28 11:44:57 +0800

    panqingqing:bug fix

--- cardequity-payment-biz/src/main/java/com/youyu/cardequity/payment/biz/CardequityPaymentApplication.java
+++ cardequity-payment-biz/src/main/java/com/youyu/cardequity/payment/biz/CardequityPaymentApplication.java
@@ -1,9 +1,8 @@
 package com.youyu.cardequity.payment.biz;
 
-import com.youyu.cardequity.common.spring.base.BaseSpringBootApplication;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.SpringCloudApplication;
 
 /**
  * @author panqingqing
@@ -11,9 +10,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * @date 2018年12月6日 下午10:00:00
  * @work 支付系统基础应用启动类
  */
-@EnableDiscoveryClient
+@SpringCloudApplication
 @MapperScan("com.youyu.cardequity.payment.biz.dal.dao")
-public class CardequityPaymentApplication extends BaseSpringBootApplication {
+public class CardequityPaymentApplication {
 
     /**
      * 主启动方法
