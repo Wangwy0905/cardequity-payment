2786feb809c996744321f7157286936f85294d83
pqqanqing <15601870402@163.com>
2019-03-29 16:27:48 +0800

    panqingqing:bug fix

--- cardequity-payment-biz/src/main/java/com/youyu/cardequity/payment/biz/component/strategy/paylog/PayLogStrategy4AlipayMobileWeb.java
+++ cardequity-payment-biz/src/main/java/com/youyu/cardequity/payment/biz/component/strategy/paylog/PayLogStrategy4AlipayMobileWeb.java
@@ -69,7 +69,9 @@ public class PayLogStrategy4AlipayMobileWeb extends PayLogStrategy {
 
     private AlipayTradeWapPayModel getAlipayTradeWapPayModel(PayLog4Alipay payLog4Alipay) {
         AlipayTradeWapPayModel alipayTradeWapPayModel = new AlipayTradeWapPayModel();
-        alipayTradeWapPayModel.setSubject(payLog4Alipay.getAlipaySubject());
+//        alipayTradeWapPayModel.setSubject(payLog4Alipay.getAlipaySubject());
+        //先写死
+        alipayTradeWapPayModel.setSubject("有鱼惠刷卡会员充值");
         alipayTradeWapPayModel.setOutTradeNo(payLog4Alipay.getAppSheetSerialNo());
         alipayTradeWapPayModel.setTotalAmount(payLog4Alipay.getOccurBalance().toString());
         alipayTradeWapPayModel.setProductCode(ALIPAY_MOBILE_WEB_PRODUCT_CODE);
