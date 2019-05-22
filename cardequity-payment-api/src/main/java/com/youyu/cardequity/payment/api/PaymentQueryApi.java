73c50ff933360334cbe9ba78e5a15afe4dec3c74
pqqanqing <15601870402@163.com>
2019-05-22 13:39:42 +0800

    增加支付查询api。

--- cardequity-payment-api/src/main/java/com/youyu/cardequity/payment/api/PaymentQueryApi.java
+++ cardequity-payment-api/src/main/java/com/youyu/cardequity/payment/api/PaymentQueryApi.java
@@ -1,9 +1,12 @@
 package com.youyu.cardequity.payment.api;
 
+import com.youyu.cardequity.common.base.annotation.ApiIdentify;
 import com.youyu.cardequity.common.base.api.BaseQueryApi;
 import io.swagger.annotations.Api;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import static com.youyu.cardequity.common.base.constant.ApiIdentifyConstant.BACKEND_IDENTIFY;
 
 /**
  * @author panqingqing
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @date 2019年5月22日 10:00:00
  * @work 支付查询Api
  */
+@ApiIdentify(value = BACKEND_IDENTIFY, desc = "只对内部执行查询")
 @Api(tags = "支付查询Api")
 @FeignClient(name = "cardequity-payment")
 @RequestMapping(path = "/paymentQuery")
